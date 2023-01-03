14f3280e7810e1431b9692f4a387f629727461bc
youweb3 <105981004+youweb3@users.noreply.github.com>
2023-01-03 18:34:58 +0000

    React Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from "./Copmonents/Header"
+import Body from "./Copmonents/Body"
+import Footer from "./Copmonents/Footer"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
