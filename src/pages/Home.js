ae4302f2eeda20599f7fe1690f756b2d05a8f276
Nikolov87 <nikolov616@gmail.com>
2023-01-07 13:07:48 +0000

    bana-update1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import first from '../pages/img/first.png'
-import Home from '../pages/Style/Home/Home.scss'
+// import Home from '../pages/Style/Home/Home.scss'
 
 
 function home() {
   return (
     <div>
-          <h1 element={Home}>A flavour for all <br />seasons</h1>
+          <h1 className='h1'>A flavour for all <br />seasons</h1>
      <img className='first' src={first} alt='' srcSet=''/>
     </div>
   )
