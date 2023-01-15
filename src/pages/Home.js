bd0aa6d1e993db90ba04fc8beaae24e3d4d99ff2
Nikolov87 <nikolov616@gmail.com>
2023-01-15 21:46:46 +0000

    adding img's teapot and the flowers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Hero from "./layout/Hero";
 import img2 from "../pages/img/img-2.png";
-// import Home from '../pages/Style/Home/Home.scss'
+import teapot from "../pages/img/teapot.png";
+import flowers from "../pages/img/flowers.png";
+
+import "../pages/Style/Home/Home.scss";
 
 function Home() {
   return (
@@ -11,7 +14,7 @@ function Home() {
       <div className="about">
         <section className="about-text">
           <h2>Our blends</h2>
-          <p className="about-text-paragraph">
+          <p className="first-paragraph">
             We make our very own Tiny Leaf blends right here in
             <br /> the tea house, offering new flavours according to
             <br /> the season. We source all our produce sustainably
@@ -20,7 +23,7 @@ function Home() {
             flavoursome cups were rewarded fairly.
           </p>
           <hr className="yelow-line" />
-          <h2 className="about-text-subheading">
+          <h2 className="text-subheading">
             Hand-picked <br /> and sustainable
           </h2>
         </section>
@@ -28,6 +31,19 @@ function Home() {
           <img className="theImg" src={img2} alt="" />
         </section>
       </div>
+      <section className="grid-container">
+        <div className="green-box"></div>
+        <div className="text-box">
+          <h3>Wise Leaf evening</h3>
+          <p>
+            Bringing together those with curious minds, speakers and observers
+            alike, our Wise Leaf events are a melting pot to share, reflect and
+            contemplate.
+          </p>
+        </div>
+        <img className="teapot-image" src={teapot} alt="" />
+        <img className="flowers-image" src={flowers} alt="" />
+      </section>
     </div>
   );
 }
