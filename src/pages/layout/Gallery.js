5b7e5239a61573e765c4f0b24a40d1934c8f3399
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-02-05 16:18:32 +0000

    adding the gallery

--- src/pages/layout/Gallery.js
+++ src/pages/layout/Gallery.js
@@ -1,30 +1,46 @@
-import gallery1 from "../img/gallery1.png"
-import gallery2 from "../img/gallery2.png"
-import gallery3 from "../img/gallery3.png"
-import gallery4 from "../img/gallery4.png"
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import gallery1 from "../img/gallery1.png";
+import gallery2 from "../img/gallery2.png";
+import gallery3 from "../img/gallery3.png";
+import gallery4 from "../img/gallery4.png";
+import "../Style/Gallery/Gallery.scss";
 
 const Gallery = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePreviousClick = () => {
+    setCurrentIndex(currentIndex - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex(currentIndex + 1);
+  };
+
   return (
-    <Carousel>
-      <div>
-      <img className="gallery-img1" src={gallery1} alt="" />
+    <>
+      <h5>Gallery</h5>
+      <div className="button-container">
+        <button className = "arrow-previous" onClick={handlePreviousClick}></button>
+        <button className = "arrow-next" onClick={handleNextClick}></button>
       </div>
-      <div>
-      <img className="gallery-img1" src={gallery2} alt="" />
-      </div>
-      <div>
-      <img className="gallery-img1" src={gallery3} alt="" />
-      </div>
-      <div>
-      <img className="gallery-img1" src={gallery4} alt="" />
-      </div>
-    </Carousel>
+      <Carousel className="theGallery" selectedItem={currentIndex} onChange={setCurrentIndex}>
+        <div>
+          <img className="gallery-img1" src={gallery1} alt="" />
+        </div>
+        <div>
+          <img className="gallery-img2" src={gallery2} alt="" />
+        </div>
+        <div>
+          <img className="gallery-img3" src={gallery3} alt="" />
+        </div>
+        <div>
+          <img className="gallery-img4" src={gallery4} alt="" />
+        </div>
+      </Carousel>
+  
+    </>
   );
-}
+};
 
 export default Gallery;
-
-
