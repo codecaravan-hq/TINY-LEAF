7a3495ee6e7737b92f0dc87647b1b47d64023bf9
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-11-07 23:39:47 +0000

    css-menu page

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,43 +1,74 @@
-import React from 'react'
+import React from "react";
 import menu from "../pages/img/menu-pic.png";
 import "../pages/Style/Menu/Menu.scss";
+
 function Menu() {
   return (
     <div>
-      <div>
-        <img className="menuHero" src={menu} alt="" />
+      <div className="menu-container">
+        <img className="menuHero" src={menu} alt="Menu Pict" />
+        <h1 className="menu-title">Menu</h1>
       </div>
-      <h1>Menu</h1>
+
       <div className="menuContainer">
         <h1 className="title">Loose leaf teas</h1>
-        
-      
+
         <h2 className="subtitle">Darjeeling</h2>
-        <article className='thetext'>
-        <p className="ptag">A refined and ligh Black tea from the region the Tibetans call “country of storms” after the thick mist that rests on the plantations</p>
-        <p className="price">£4.50</p>
+        <article className="thetext">
+          <p className="ptag">
+            A refined and ligh Black tea from the region the Tibetans call
+            “country of storms” after the thick mist that rests on the
+            plantations
+          </p>
+          <p className="price">£4.50</p>
         </article>
 
-        <p className="ptag">Description 1</p>
-        
-        <p className="price">Price 1</p>
-        <h2 className="subtitle">Subtitle 2</h2>
-        <p className="ptag">Description 2</p>
-        <p className="price">Price 2</p>
-        <h2 className="subtitle">Subtitle 3</h2>
-        <p className="ptag">Description 3</p>
-        <p className="price">Price 3</p>
-        <h2 className="subtitle">Subtitle 4</h2>
-        <p className="ptag">Description 4</p>
-        <p className="price">Price 4</p>
-        <h2 className="subtitle">Subtitle 5</h2>
-        <p className="ptag">Description 5</p>
-        <p className="price">Price 5</p>
-        <h2 className="subtitle">Subtitle 6</h2>
-        <p className="ptag">Description 6</p>
-        <p className="price">Price 6</p>
+        <h2 className="subtitle">Blue Earl Grey</h2>
+        <article className="thetext">
+          <p className="ptag">
+            A delicate blend of blueberry flower and sunflower petals with Earl
+            Grey aromas
+          </p>
+          <p className="price">£4.50</p>
+        </article>
+
+        <h2 className="subtitle">Organic Mango and Papaya</h2>
+        <article className="thetext">
+          <p className="ptag">
+            A mix of Indian Black tea, Ceylan, Assam and Chinese Green tea,
+            along with dried mango and papaya for a sweet exotic flavour
+          </p>
+          <p className="price">£5</p>
+        </article>
+
+        <h2 className="subtitle">White Magic</h2>
+        <article className="thetext">
+          <p className="ptag">
+            White tea sprinkled with rose buttons and marigold, with aromas of
+            passion fruit, peach and apricot
+          </p>
+          <p className="price">£5</p>
+        </article>
+
+        <h2 className="subtitle">Wise Flower</h2>
+        <article className="thetext">
+          <p className="ptag">
+            An unusual blend of rose, Sencha Green tea, dates and gooseberry
+            petals. Refreshing and seasonal.
+          </p>
+          <p className="price">£5</p>
+        </article>
+
+        <h2 className="subtitle">Night at the Palace</h2>
+        <article className="thetext">
+          <p className="ptag">
+            Earl Grey aromas, kiwi, yellow peach, orange blossom and violet make
+            this one of our most exquisite blends
+          </p>
+          <p className="price">£5.50</p>
+        </article>
       </div>
     </div>
-  )
+  );
 }
-export default Menu
+export default Menu;
