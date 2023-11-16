fa78ed42a086f1743f890cd5e7515fef65b08f70
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-11-16 05:28:28 +0000

    css/form

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import reservations from "../pages/img/reserv.png";
 import "../pages/Style/Reservation/Reservation.scss";
 
 const Reservation = () => {
@@ -18,75 +18,116 @@ const Reservation = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
-      <div>
-        <label>
-          Date:
-          <input
-            type="text"
-            value={date}
-            onChange={(event) => setDate(event.target.value)}
-          />
-        </label>
-        <label>
-          Time:
-          <input
-            type="text"
-            value={time}
-            onChange={(event) => setTime(event.target.value)}
-          />
-        </label>
+    <>
+      <section className="menu-container">
+        <img className="menuHero" src={reservations} alt="Reserv Pict" />
+        <h1 className="menu-title">Reservations</h1>
+      </section>
+      <div className="container-reservat">
+        <section className="top-text">
+          <h2 className="reserv-sub">Reserve a table</h2>
+          <article className="thetext">
+            <p className="ptag-reserv">
+              We take bookings for groups up to 6. We get busy on weekends so
+              best to book early to avoid disappointment!
+            </p>
+          </article>
+        </section>
+
+        <form onSubmit={handleSubmit}>
+          <h1>{title}</h1>
+          <h2>{subtitle}</h2>
+          <article className="dateTimeGroup">
+            <div>
+              <label>
+                Date
+                <br />
+                <input
+                  className="dateTimeGroupSize"
+                  type="text"
+                  value={date}
+                  onChange={(event) => setDate(event.target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Time
+                <br />
+                <input
+                  className="dateTimeGroupSize"
+                  type="text"
+                  value={time}
+                  onChange={(event) => setTime(event.target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Group Size
+                <br />
+                <input
+                  className="dateTimeGroupSize"
+                  type="text"
+                  value={groupSize}
+                  onChange={(event) => setGroupSize(event.target.value)}
+                />
+              </label>
+            </div>
+          </article>
+          <article className="nameContactNumber">
+            <div>
+              <label>
+                Name
+                <br />
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                  id="name"
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Contact Number
+                <br />
+                <input
+                  type="text"
+                  value={contactNumber}
+                  onChange={(event) => setContactNumber(event.target.value)}
+                  id="contact"
+                />
+              </label>
+            </div>
+          </article>
+          <div>
+            <label>
+              Comment / Request
+              <br />
+              <textarea
+                value={comment}
+                onChange={(event) => setComment(event.target.value)}
+                id="commentRequest"
+              />
+            </label>
+          </div>
+          <div className="conditions">
+            <input
+              type="checkbox"
+              checked={acceptTerms}
+              onChange={(event) => setAcceptTerms(event.target.checked)}
+              id="checkbox"
+            />
+            <p className="agreement">
+              By requesting this booking, I am accepting Tiny Leaf's Terms and
+              Conditions.
+            </p>
+          </div>
+          <button type="submit">Reserve</button>
+        </form>
       </div>
-      <div>
-        <label>
-          Group Size:
-          <input
-            type="text"
-            value={groupSize}
-            onChange={(event) => setGroupSize(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-        </label>
-        <label>
-          Contact Number:
-          <input
-            type="text"
-            value={contactNumber}
-            onChange={(event) => setContactNumber(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Comment/Request:
-          <textarea
-            value={comment}
-            onChange={(event) => setComment(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          checked={acceptTerms}
-          onChange={(event) => setAcceptTerms(event.target.checked)}
-        />
-        By requesting this booking, I am accepting Tiny Leaf's Terms and
-        Conditions.
-      </div>
-      <button type="submit">Reserve</button>
-    </form>
+    </>
   );
 };
 
